102b303cab7b77ccd7f101072122aba3463a17fa
Babatunde50 <babatundeololade50@gmail.com>
2022-03-30 02:19:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require('./src/app');
+const pool = require('./src/pool');
+
+pool
+  .connect({
+    host: 'localhost',
+    port: 5432,
+    database: 'socialnetwork',
+    user: 'babatundeololade',
+    password: '',
+  })
+  .then(() => {
+    app().listen(3005, () => {
+      console.log('Listening on port 3005');
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ migrations/1648590298272_add-users-table.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            bio VARCHAR(400),
+            username VARCHAR(30) NOT NULL
+        )
+    `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`
+        DROP TABLE users
+    `);
+};
+
+// DATABASE_URL=postgres://babatundeololade@localhost:5432/socialnetwork npm run migrate up
